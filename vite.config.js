bfb53f4dd39bec747dab4e013db34653550e2072
TheReliableMart <thereliablemart@gmail.com>
2024-11-13 22:33:26 +0500

    S

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     include: ['@react-pdf-viewer/core', '@react-pdf-viewer/pdfjs'],
-//   },
-// });
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['@react-pdf-viewer/core', '@react-pdf-viewer/pdfjs'],
-  },
-  server: {
-    host: '0.0.0.0',  // This will make the server accessible on any network interface
-    // port: 3000,  // You can specify the port here
-  },
-});
+  base: '/TechnicalWritingAndPresentationSkills-A/', // Set this to your repo name
+})
